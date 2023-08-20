9cf6addbdc4effa8d4bd3fb3f08bd8399a43dc5a
Gabriela Pascoal <gabypascoal9@gmail.com>
2023-08-20 12:44:26 -0300

    Adicionando obstáculo.

---
+++ Platformer/Obstacle.h
@@ -0,0 +1,38 @@
+#ifndef _PLATFORMER_OBSTACLE_H_
+#define _PLATFORMER_OBSTACLE_H_
+
+// ---------------------------------------------------------------------------------
+// Inclusões
+
+#include "Types.h"            // tipos específicos da engine
+#include "Object.h"           // interface de object
+#include "Sprite.h"           // interface de sprites
+#include "Image.h"            // interface de image
+
+// ---------------------------------------------------------------------------------
+
+class Obstacle : public Object
+{
+private:
+    Sprite* sprite;
+    float vel;
+
+public:
+    Obstacle(Image* img, float speed);
+    ~Obstacle();
+
+    void Update();
+    void Draw();
+};
+
+// ---------------------------------------------------------------------------------
+// Funções Membro Inline
+
+inline void Obstacle::Draw()
+{
+    sprite->Draw(x, y, z);
+}
+
+// ---------------------------------------------------------------------------------
+
+#endif
